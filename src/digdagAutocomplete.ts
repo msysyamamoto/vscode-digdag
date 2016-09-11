8ec7ee3c441fe7bdb2aac24a9b69508d78eef4d2
Masayasu Yamamoto <m2yamamoto@gmail.com>
2016-09-11 23:27:24 +0900

    Support for intellisense

---
+++ src/digdagAutocomplete.ts
@@ -0,0 +1,69 @@
+'use strict';
+
+import {CompletionItemProvider, CompletionItem, CompletionItemKind, CancellationToken, TextDocument, Position, Range} from 'vscode';
+
+export class DigdagAutocomplete implements CompletionItemProvider {
+
+	private keywords = [
+        "!include :",
+        "_background:",
+        "_check:",
+        "_command:",
+        "_do:",
+        "_error:",
+        "_export:",
+        "_parallel:",
+        "_retry:",
+        "_type:",
+        "alert:",
+        "duration:",
+        "fail:",
+        "schedule:",
+        "sla:",
+        "time:"
+	];
+
+	private variables = [
+		"timezone",
+		"session_uuid",
+		"session_time",
+		"session_date",
+		"session_date_compact",
+		"session_local_time",
+		"session_tz_offset",
+		"session_unixtime",
+		"last_session_time",
+		"last_session_date",
+		"last_session_date_compact",
+		"last_session_local_time",
+		"last_session_tz_offset",
+		"last_session_unixtime",
+		"next_session_time",
+		"next_session_date",
+		"next_session_date_compact",
+		"next_session_local_time",
+		"next_session_tz_offset",
+		"next_session_unixtime",
+	]
+
+    provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Promise<CompletionItem[]> {
+
+		var range = document.getWordRangeAtPosition(position);
+		var prefix = range ? document.getText(range) : '';
+		var result = this.makeProposals(prefix, this.keywords, CompletionItemKind.Keyword)
+			.concat(this.makeProposals(prefix, this.variables, CompletionItemKind.Variable));
+
+		return Promise.resolve(result);
+	}
+
+	makeProposals(prefix: string, labels:string[], kind:CompletionItemKind): CompletionItem[] {
+		return labels.filter((label) => {
+			return prefix.length === 0
+				|| (label.length >= prefix.length && label.substr(0, prefix.length) === prefix);
+		}).map((label) => {
+			var item = new CompletionItem(label);
+			item.kind = kind;
+			return item;
+		});
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DigdagAutocomplete } from './digdagAutocomplete';
+
+const LANG_DIGDAG: vscode.DocumentFilter = { language: 'digdag', scheme: 'file' };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(LANG_DIGDAG, new DigdagAutocomplete)
+    );
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-digdag" is now active!');
-
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+	// need to set in the extension host as well as the completion provider uses it.
+	vscode.languages.setLanguageConfiguration('digdag', {
+		wordPattern: /[^\-\`\~\@\#\%\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+/g
+	});
 }
